b10f7b423b1797408af18545e2a5c768bfea6677
khaled battok <kh.ba5540@gmail.com>
2020-08-11 21:34:27 +0430

    Hazard Fix

--- Hazard_detection.v
+++ Hazard_detection.v
@@ -5,17 +5,15 @@ module Hazard_detection (
 );
 
 always @(*) begin
-    if(exe_wb_en)
-        hazard_output=(src1 == exe_dest);
-    else if(mem_wb_en)
-        hazard_output=(src1 == mem_dest);
-    else if(two_src && exe_wb_en)
-        hazard_output=(src2 == exe_dest);
-    else if(two_src && mem_wb_en)
-        hazard_output=(src2 == mem_dest);
+    if(exe_wb_en && (src1 == exe_dest))
+        hazard_output=1'b1;
+    else if(mem_wb_en && (src1 == mem_dest))
+        hazard_output=1'b1;
+    else if(two_src && exe_wb_en && (src2 == exe_dest))
+        hazard_output=1'b1;
+    else if(two_src && mem_wb_en && (src2 == mem_dest))
+        hazard_output=1'b1;
     else
         hazard_output=1'b0;
-
-    
 end
 endmodule
